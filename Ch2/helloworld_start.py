5a0259138688bed8bc0d3801f88884ce66970b55
Gead Kee <gangeadkee@gmail.com>
2022-07-04 21:39:28 +0800

    added a few files

---
+++ Ch2/helloworld_start.py
@@ -0,0 +1,13 @@
+#
+# Example file for HelloWorld
+#
+
+def main(): 
+    print("Hello World!!");
+    # name = input("Whats ur name?");
+    # print("Nice to meet u, ", name);
+    # age = input("How old are you?");
+    # print("So u are ", age, "yrs old, and called ", name)
+
+if __name__ == "__main__":      # for the terminal to check if this program is the right one before executing the function 
+    main();
