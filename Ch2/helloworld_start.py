27141fb60df01eb7e060331c76b0c6017375a9e0
Gead Kee <gangeadkee@gmail.com>
2022-07-07 21:41:51 +0800

    helloworld and variables

--- Ch2/helloworld_start.py
+++ Ch2/helloworld_start.py
@@ -4,10 +4,10 @@
 
 def main(): 
     print("Hello World!!");
-    # name = input("Whats ur name?");
-    # print("Nice to meet u, ", name);
-    # age = input("How old are you?");
-    # print("So u are ", age, "yrs old, and called ", name)
+    name = input("Whats ur name?");
+    print("Nice to meet u, ", name);
+    age = input("How old are you?");
+    print("So u are ", age, "yrs old, and called ", name);
 
 if __name__ == "__main__":      # for the terminal to check if this program is the right one before executing the function 
     main();
--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -2,12 +2,39 @@
 # Example file for variables
 #
 
-# Declare a variable and initialize it
+from xmlrpc.client import boolean
 
+
+# Declare a variable and initialize it
+num = 5;
+float1 = 1.45;
+string1 = "Collection of characters";
+boolean1 = True;
+list1 = [1, False, 34.67, "two", 4, "five"];
+tuple1 = (0, 1, 2);
+dictionary1 = {"one": 1, "two": 2};
+
+print(num);
+print(float1);
+print(string1);
+print(list1);
+print(tuple1);
+print(dictionary1);
 
 
 # re-declaring the variable works
+num = "abc";
+print(num);
 
+# to access a member of a sequence type, []
+print(tuple1[1]);
+print(list1[2]);
+print(distionary1[0])
+
+
+# use slices to access part of the sequence
+print(list1[1:4])
+print(list1[1:5:2]) #step value of ":2"
 
 
 # ERROR: variables of different types cannot be combined
