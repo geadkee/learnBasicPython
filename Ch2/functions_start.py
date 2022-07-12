64c223fce904e4cd474e8711e16c06feb4f53ec4
Gead Kee <gangeadkee@gmail.com>
2022-07-12 19:58:57 +0800

    functions done

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -6,16 +6,52 @@
 def function1():
     print("A basic function.");
 
-# function that takes arguments
 
+# function that takes arguments
+def function2(r):
+    total = 5 + r;
+    print(total);
+    
 
 # function that returns a value
+def function3(s, t):
+    total = s + t;
+    return(print(total));
+    # return s + t;
 
 
 # function with default value for an argument
+def function4(p, q = 1):
+    result = 1;
+    for i in range(q):
+        result = result + q;
+    return result;
+
+def function5(p, q = 1):
+    result = 1;
+    for i in range(q):
+        result = result * p;
+    return result;
 
 
 #function with variable number of arguments
+def function6(*arg):
+    result = 0;
+    for i in arg:
+        result = result + i;
+    return result;
 
+# function1();
+# print(function1());
+# print(function1);
 
-function1();
+# function2(6);
+# function3(3, 7);
+# print(function3(3, 7));
+
+print(function4(4));
+print(function4(4, 5));
+
+print(function5(4, 5));
+
+print(function6(4, 5, 10, 4, 10));
