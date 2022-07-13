208cba0a41fe4a2f26a4cba1ef3f3132aac673ee
Gead Kee <gangeadkee@gmail.com>
2022-07-13 13:33:36 +0800

    conditions

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -3,13 +3,24 @@
 #
 
 
-def main():
-    x, y = 10, 100
+from unittest import result
+
+
+def main(x, y):
 
     # conditional flow uses if, elif, else
+    if(x < y):
+        result = "x less than y"
+    elif(x == y):
+        result = "x equal y"
+    else:
+        result = "x greater than y"
+
+    print(result)
 
     # conditional statements let you use "a if C else b"
-
+    result = "equal" if x ==y else "not equal";
+    print(result);
 
 if __name__ == "__main__":
     main()
