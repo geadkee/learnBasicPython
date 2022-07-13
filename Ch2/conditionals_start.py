b210afd46146425f9dc4270721cd5008ac9a9ebc
Gead Kee <gangeadkee@gmail.com>
2022-07-13 13:50:35 +0800

    conditions done

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -2,10 +2,6 @@
 # Example file for working with conditional statements
 #
 
-
-from unittest import result
-
-
 def main(x, y):
 
     # conditional flow uses if, elif, else
@@ -19,8 +15,23 @@ def main(x, y):
     print(result)
 
     # conditional statements let you use "a if C else b"
-    result = "equal" if x ==y else "not equal";
+    result = "equal" if x == y else "not equal";
     print(result);
 
+    # match-case makes it easy to compare multiple values
+    match y:
+        case "one":
+            result = 1;
+        case "two":
+            result = 2;
+        case "three" | "four":
+            result = (3, 4);
+        case 5 | 6:
+            result = ("five", "six");
+        case _:
+            result = "can't detect, out of bounds"
+    print(result);
+
+
 if __name__ == "__main__":
-    main()
+    main(3, 5)
