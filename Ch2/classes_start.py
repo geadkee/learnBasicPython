745a89a12f90ce7f770ca791579f989f612b2c4e
Gead Kee <gangeadkee@gmail.com>
2022-07-16 21:29:50 +0800

    fixed errors in class

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -7,8 +7,9 @@ class Vehicles():
         self.bodystyle = bodystyle;     #"Vehicle()" class has "bodystyle" property
     wheels = 3;
 
-    def Drive(self, speed, distance, time):
-        self.speed = speed
+    def Drive(self, distance, time):    # defining a function in a class
+        self.distance = distance
+        self.time = time
         self.mode = "driving"
 
 class Car(Vehicles):
@@ -19,9 +20,10 @@ class Car(Vehicles):
         self.cylinder = 4
         self.enginetype = enginetype
 
-    def Drive(self, speed, distance, time):
-        super().Drive(speed, distance, time)
-        return print("Driving my ", self.enginetype, self.enginetype ," at speed ", self.speed)
+    def Drive(self, distance, time):    #derive a function from it's parent class
+        super().Drive(distance, time)
+        self.speed = distance/time
+        return print("Driving my ", self.enginetype, self.bodystyle ," at speed ", self.speed, "meter/min")
 
 class Bike(Vehicles):
     def __init__(self, enginetype, hasSideCar):
@@ -34,6 +36,11 @@ class Bike(Vehicles):
         self.hasSideCar = hasSideCar
         self.cylinder = 2
 
+    def Drive(self, distance, time):    #derive a function from it's parent class
+        super.Drive(distance, time)
+        self.speed = distance/time;
+        return print("Riding my ", self.enginetype, self.bodystyle, " at speed ", self.speed, "meter/min")
+
 car1 = Car("Manual");
 car2 = Car("Auto");
 bike1 = Bike("Gas", True);
@@ -43,13 +50,13 @@ print(car2.enginetype)
 print(car1.wheels, car1.wheel)
 print(bike1.hasSideCar)
 
-car1.Drive(60)
-car2.Drive(120)
+car1.Drive(60, 10)
+car2.Drive(120, 10)
 
 
-def main():
-    print("classes")
+# def main():
+#     print("classes")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
