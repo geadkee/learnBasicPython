55ffd45c82f709abb0f627a39bfaf56c5b76a651
Gead Kee <gangeadkee@gmail.com>
2022-07-16 17:43:04 +0800

    classes, OOP-concept

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -2,8 +2,24 @@
 # Example file for working with classes
 #
 
+class Vehicles():
+    def __init__(self, bodystyle):
+        self.bodystyle = bodystyle;
+    wheels = 4;
+    engine = 1;
+
+class Car(Vehicles):
+    def __init__(self, enginetype):
+        super().__init__("Car")
+        self.wheel = 4
+        self.doors = 4
+        self.cylinder = 4
+        self.enginetype = enginetype
+
+
 
 def main():
+    print("classes")
 
 
 if __name__ == "__main__":
