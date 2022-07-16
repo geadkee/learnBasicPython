b040789bae45082c4371c620d16dd299b539e4f9
Gead Kee <gangeadkee@gmail.com>
2022-07-16 21:50:29 +0800

    modules importing

--- Ch2/modules_start.py
+++ Ch2/modules_start.py
@@ -1,10 +1,13 @@
 # import the math module, which contains features for working with mathematics
-
+import math
 
 # the math module contains lots of pre-built functions
-
+def SquareRoot(x):
+    return print("Square root of ", x, "is", math.sqrt(x)) 
 
 # in addition to functions, some modules contain useful constants
-
+print("the numbers of Pi is ", math.pi)
 
 # try some of the math functions for yourself here:
+
+SquareRoot(4)
