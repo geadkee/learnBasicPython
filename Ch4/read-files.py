23053bf1be33f6b3895ee9bcbeaa15add2aa7b72
Gead Kee <gangeadkee@gmail.com>
2022-07-21 17:37:27 +0800

    read files

--- Ch4/read-files.py
+++ Ch4/read-files.py
@@ -6,8 +6,13 @@ def main():
 
     myfile = open("textfile.txt", "r")
     if myfile.mode == "r":              #to check if the file is being opened correctly
+
         readContents = myfile.read()
         print(readContents)
+
+        fl = myfile.readlines()
+        for i in fl:
+            print(i)
 
 
 
