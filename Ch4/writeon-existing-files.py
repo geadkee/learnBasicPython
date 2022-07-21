6e80392b24a37462f9770b07d7d77a10f39a37f9
Gead Kee <gangeadkee@gmail.com>
2022-07-21 17:19:09 +0800

    append data to file

---
+++ Ch4/writeon-existing-files.py
@@ -0,0 +1,17 @@
+#
+# Read and write files using the built-in Python file methods
+#
+
+def main():
+
+    # Open the file for appending text to the end
+    myfile = open("textfile.txt", "a")      #"a" == append data to the file
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main();
