c8dc146c67aea24ad3ec50cbf5f718984974bbb1
Gead Kee <gangeadkee@gmail.com>
2022-07-22 10:50:44 +0800

    file paths utilities, file mod datetime

--- Ch4/ospathutils_start.py
+++ Ch4/ospathutils_start.py
@@ -1,6 +1,7 @@
 #
 # Example file for working with os.path module
 #
+from distutils.text_file import TextFile
 import os
 from os import path
 import datetime
@@ -17,14 +18,18 @@ def main():
   print("It is a File: ", path.isfile("textfile.txt"))
   print("It is a Directory: ", path.isdir("textfile.txt"))
 
-  
   # Work with file paths
-
+  print("Item's path: ", path.realpath("txtfile.txt"))
+  print("Item's path & name: ", path.split(path.realpath("txtfile.txt")))
   
   # Get the modification time
-
+  t = time.ctime(path.getmtime("textfile.txt"))
+  print(t)
+  print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
   
   # Calculate how long ago the item was modified
+  md = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"))
+  print("It has been ", md, "since the file was modified.")
 
   
 if __name__ == "__main__":
